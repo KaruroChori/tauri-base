c1a8572ec53b734e8ecea3038fee50508e874c44
karurochari <nope>
2022-07-02 14:46:43 +0000

    i18n support in vue

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const i18n = createI18n({})
+const app = createApp(App)
+
+app.use(i18n).mount('#app')
