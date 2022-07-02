c4017af539e84c18599ec7d3e0a5db74da75c45e
karurochari <nope>
2022-07-02 18:10:26 +0000

    SASS support

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,3 +1,10 @@
 <template lang="pug">
 h2 hello world!
 </template>
+
+<style scoped lang="scss">
+h2{
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: lightcoral;
+}
+</style>
